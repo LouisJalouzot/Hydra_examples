e48e8e2ad6c85e4a57cfdd82fed641bc62556d94
Louis Jalouzot <ljalouzot1@gmail.com>
2024-12-11 18:16:00 +0100

    Hydra and job log to file in addition to console and job log only to file in multirun

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import logging
+from time import sleep
+
 import hydra
 from omegaconf import DictConfig
 from sklearn.datasets import make_classification
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+
+logger = logging.getLogger(__name__)
 
 
 @hydra.main(
@@ -12,7 +17,7 @@ from sklearn.model_selection import train_test_split
 def main(cfg: DictConfig):
     X, y = make_classification(random_state=0)
     X_train, X_val, y_train, y_val = train_test_split(
-        X, y, test_size=0.2, random_state=42
+        X, y, test_size=0.2, random_state=0
     )
     model = RandomForestClassifier(
         criterion=cfg.criterion,
@@ -23,6 +28,8 @@ def main(cfg: DictConfig):
     model.fit(X_train, y_train)
     val_predicted = model.predict(X_val)
     val_accuracy = accuracy_score(y_val, val_predicted)
+
+    logger.info(f"Validation accuracy: {val_accuracy:.3g}")
 
     return val_accuracy
 
